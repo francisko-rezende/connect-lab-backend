fa041b958dc721bc5cf68bc037bc8ea11ea9e65a
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-01-07 16:39:05 -0300

    fix: add missing @IsNotEmpty decorators to credetials dto

--- src/core/auth/dto/credentials.dto.ts
+++ src/core/auth/dto/credentials.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CredentialsDto {
   @IsString()
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @IsString()
   @MinLength(8)
+  @IsNotEmpty()
   readonly password: string;
 }
