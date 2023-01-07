cdc45c582e68aa472dad230904118717d92b12f6
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-01-07 16:38:36 -0300

    fix: add missing @IsNotEmpty decorators to change-password dto

--- src/core/auth/dto/change-password.dto.ts
+++ src/core/auth/dto/change-password.dto.ts
@@ -1,20 +1,24 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 import { MatchesOtherField } from 'src/core/constraints/matches-other-field.decorator';
 
 export class ChangePasswordDto {
   @IsString()
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @IsString()
   @MinLength(8)
+  @IsNotEmpty()
   readonly oldPassword: string;
 
   @IsString()
   @MinLength(8)
+  @IsNotEmpty()
   readonly newPassword: string;
 
   @IsString()
+  @IsNotEmpty()
   @MatchesOtherField('newPassword', {
     message: 'confirmNewPassword field must match newPassword field',
   })
