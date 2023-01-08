06f6b608bb046a174457ab5b1cd58491af92f755
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-01-08 11:52:57 -0300

    feat: create endpoints for creating userDevices/linking user devices and users and for listing a given user devices

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,5 @@
+import { UserEntity } from 'src/entities/user.entity';
+import { UserDeviceEntity } from './entities/userDevice.entity';
 import { devices } from './seeds/device-seeds';
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
@@ -8,6 +10,10 @@ export class AppService {
   constructor(
     @Inject('DEVICE_REPOSITORY')
     private readonly deviceRepository: Repository<DeviceEntity>,
+    @Inject('USER_REPOSITORY')
+    private readonly userRepository: Repository<UserEntity>,
+    @Inject('USER_DEVICE_REPOSITORY')
+    private readonly userDeviceRepository: Repository<UserDeviceEntity>,
   ) {}
 
   populateDb() {
@@ -24,6 +30,56 @@ export class AppService {
         await this.deviceRepository.save(createdDevices);
         resolve('Default devices added to the database');
       } catch (error) {
+        reject({ detail: error.detail, code: error.code });
+      }
+    });
+  }
+
+  linkDevice(userId: number, deviceId: number) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await this.userRepository.findOne({
+          where: { userId: userId },
+        });
+
+        const device = await this.deviceRepository.findOne({
+          where: { deviceId: deviceId },
+        });
+
+        const newUserDevice = this.userDeviceRepository.create({
+          user,
+          device,
+        });
+        await this.userDeviceRepository.save(newUserDevice);
+        console.log(newUserDevice);
+        resolve(newUserDevice);
+      } catch (error) {
+        reject({ detail: error.detail, code: error.code });
+      }
+    });
+  }
+
+  findAllUserDevices(userId: number) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await this.userRepository.findOne({
+          where: { userId: userId },
+          relations: { userDevices: { device: { deviceInfo: true } } },
+        });
+
+        const userDevices = user.userDevices.map(
+          ({ device: { name, type, madeBy, deviceInfo }, isOn }) => ({
+            name,
+            type,
+            madeBy,
+            isOn,
+            deviceInfo,
+          }),
+        );
+
+        resolve(userDevices);
+      } catch (error) {
+        console.log(error);
         reject({ detail: error.detail, code: error.code });
       }
     });
