2c0d9027183743d27f76b6bfbfeff33893be1c1c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-01-09 09:32:53 -0300

    feat: add room prop and relation with locations to user-device.entity

--- src/entities/user-device.entity.ts
+++ src/entities/user-device.entity.ts
@@ -7,6 +7,7 @@ import {
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { LocationEntity } from './location.entity';
 
 @Entity({ name: 'user_devices' })
 export class UserDeviceEntity {
@@ -15,6 +16,9 @@ export class UserDeviceEntity {
 
   @Column({ default: false })
   isOn: boolean;
+
+  @Column()
+  room: string;
 
   @ManyToOne(() => UserEntity, (userEntity) => userEntity.userDevices, {
     onDelete: 'SET NULL',
@@ -29,4 +33,11 @@ export class UserDeviceEntity {
   })
   @JoinColumn({ name: 'device_id' })
   device: DeviceEntity;
+
+  @ManyToOne(() => LocationEntity, (location) => location.userDevices, {
+    onDelete: 'SET NULL',
+    cascade: true,
+  })
+  @JoinColumn({ name: 'location_id' })
+  location: LocationEntity;
 }
